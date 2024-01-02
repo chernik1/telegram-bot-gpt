ba1088d8f77125bd1b0fc749509e68b0fc37a62b
chernik1 <vvvvf-vvv@mail.ru>
2024-01-02 22:38:22 +0300

    update

--- bot/test.py
+++
@@ -1,8 +0,0 @@
-with open('response.txt', 'r', encoding='utf-8') as file:
-    text = file.read()
-
-import re
-pattern = r"\d{1,3}!(.+?)(?=\d|$)"
-matches = re.findall(pattern, text)
-for match in matches:
-    print(match)
